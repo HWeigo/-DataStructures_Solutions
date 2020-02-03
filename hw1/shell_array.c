80df993339af8813ae0d7fd3b7dee1e4fd7244f9
HWeigo <746144870@qq.com>
2020-02-03 13:11:48 +0800

    create shell_array.c

---
+++ hw1/shell_array.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <shell_array.h>
+
+long *Array_Load_From_File(char *filename, int *size)
+{
+
+}
+
+int main(int argc, char ** argv)
+{
+	//argv[1]: name of input file (binary)
+	//argv[2]: name of output file (binary)
+	if(argc != 3)
+	{
+		return EXIT_FAILURE;
+	}
+
+
+}
+
