4f0506f6421698c0f0dfc7ef5857b44b0c837f82
HWeigo <746144870@qq.com>
2020-02-03 13:52:27 +0800

    count number and gcc maker

---
+++ hw1/gcc.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+gcc -O3 -std=c99 -Wall -Wshadow -Wvla -pedantic shell_array.c -o test
--- hw1/shell_array.c
+++ hw1/shell_array.c
@@ -2,11 +2,35 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
-#include <shell_array.h>
+#include "shell_array.h"
+
+int countIntegers(char *filename)
+{
+	FILE *fptr;
+	fptr = fopen(filename, "r");
+	
+	if(fptr == NULL)
+	{
+		fprintf(stderr, "fopen failed.");
+		return -1;
+	}
+
+	long tmp;
+	int cnt  = 0;
+	while(fread(&tmp, sizeof(long), 1,fptr))
+	{
+		cnt++;
+	}
+
+	fclose(fptr);
+	return cnt;
+}
 
 long *Array_Load_From_File(char *filename, int *size)
 {
-
+	long *lptr = NULL;
+	*lptr = 1;
+	return lptr;
 }
 
 int main(int argc, char ** argv)
@@ -18,6 +42,9 @@ int main(int argc, char ** argv)
 		return EXIT_FAILURE;
 	}
 
+	printf("cnt = %d\n", countIntegers(argv[1]));
+	
+	return EXIT_SUCCESS;
 
 }
 
