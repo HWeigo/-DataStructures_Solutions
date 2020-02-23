ea6d6e1927a92417ce082632e577732a25f0ac07
HWeigo <746144870@qq.com>
2020-02-24 00:24:54 +0800

    table construct(not test yet)

--- hw2/encode.c
+++ hw2/encode.c
@@ -5,6 +5,10 @@
 #include "encode.h"
 
 static ListNode *ListNodeConstruct(int value);
+static void Push(Stack *s, int value);
+static void Pop(Stack *s);
+static void PrintStack(Stack *s);
+void Encode(TreeNode *root, Stack *trail, int **table);
 
 int **ConstructTable(TreeNode *root)
 {
@@ -16,15 +20,51 @@ int **ConstructTable(TreeNode *root)
 	{
 		table[i] = malloc(sizeof(int) * treeHeight);
 	}
+
+	// Initialization
+	for(int i=0;i<256;i++)
+	{
+		for(int j=0;j<treeHeight;j++)
+		{
+			table[i][j] = -1;
+		}
+	}
 	
 	Stack *trail = malloc(sizeof(*trail));
 	trail->head = NULL;
 	trail->tail = NULL;
 
+	Encode(root, trail, table);
+	
 	free(trail);
 
 	return table;
 }
+
+void Encode(TreeNode *root, Stack *trail, int **table)
+{
+	printf("%c: %ld\n", root->charIdx, root->freq);
+	if(root->charIdx != -1)
+	{
+		PrintStack(trail);
+		ListNode *head = trail->head;
+		int i=0;
+		while(head != NULL)
+		{
+			table[root->charIdx][i] = head->value;
+			head = head->next;
+		}
+		Pop(trail);
+		return;
+	}
+	Push(trail, 0);
+	Encode(root->left, trail, table);
+	Push(trail, 1);
+	Encode(root->right, trail, table);
+
+	Pop(trail);
+}
+
 
 void FreeTable(int **table)
 {
@@ -35,7 +75,7 @@ void FreeTable(int **table)
 	free(table);
 }
 
-void Push(Stack *s, int value)
+static void Push(Stack *s, int value)
 {
 	if(s->head == NULL)
 	{
@@ -47,19 +87,19 @@ void Push(Stack *s, int value)
 	s->tail = (s->tail)->next;
 }
 
-int Pop(Stack *s)
+static void Pop(Stack *s)
 {
 	if(s->head == NULL)
 	{
-		return -1;
+		return;
 	}
 	if(s->head == s->tail)
 	{
-		int topValue = (s->head)->value;
+		//int topValue = (s->head)->value;
 		free(s->head);
 		s->head = NULL;
 		s->tail = NULL;
-		return topValue;
+		return;
 	}
 	
 	ListNode *p = s->head;
@@ -71,11 +111,23 @@ int Pop(Stack *s)
 	}
 
 	// q is now tail of stack, p is the node in front of q
-	int topValue = q->value;
+	//int topValue = q->value;
 	p->next = NULL;
+	s->tail = p;
 	free(q);
 
-	return topValue;
+	return;
+}
+
+static void PrintStack(Stack *s)
+{
+	ListNode *head = s->head;
+	while(head != NULL)
+	{
+		printf("%d ", head->value);
+		head = head->next;
+	}
+	printf("\n");
 }
 
 static ListNode *ListNodeConstruct(int value)
