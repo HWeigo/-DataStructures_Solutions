71e5b5c424062499a35317f6c8bfea3d2cf42b3c
HWeigo <746144870@qq.com>
2020-04-12 06:00:33 +0800

    update

--- hw5_Merge_And_Quick_Sort/sorting.c
+++ hw5_Merge_And_Quick_Sort/sorting.c
@@ -10,3 +10,43 @@ void Quick_Sort(long *Array, int Size)
 }
 
 
+static void QuickSortHelper(long *array, int lb, int ub)
+{
+	if(lb >= ub)
+	{
+		return;
+	}
+	int patitionIdx = Patition(array, lb, ub);
+	QuickSortHelper(array, lb, patitionIdx-1);
+	QuickSortHelper(array, patitionIdx +1, ub);
+}
+
+static int Patition(long *array, int lb, int ub)
+{
+	pivot = ;
+	down = lb;
+	up = ub;
+	while(down < up)
+	{
+		while(array[down] > pivot)
+		{
+			down++;
+		}
+		while(array[up] > pivot)
+		{
+			up--;
+		}
+		if(down < up)
+		{
+			long tmp = array[down];
+			array[down] = array[up];
+			array[up] = tmp;
+		}
+	}
+	return up;
+}
+
+void Merge Sort(long *Array, int Size)
+{
+
+}
